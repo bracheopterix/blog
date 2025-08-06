c29a7e2ae9b42a16669d21a187eb4f0eb1c5429f
bracheopterix <bracheopterix@gmail.com>
2025-08-06 19:39:24 +0300

    pause

--- src/TimeTracker/TimeTracker.tsx
+++ src/TimeTracker/TimeTracker.tsx
@@ -28,6 +28,13 @@ function TimeTracker() {
     // save in (to the serv) , save out triggers => calc delta and save the record to the "server", clean "in";
     // We should have a clock on the panel that is counting time from the last "in" if it exists;
 
+
+    // Inside the adaptive superContainer in the length of ALL the loaded records 
+    // we have realList which only renders what we do need. 
+    // and somewhat of a scroll catcher?
+
+
+
     // GREETINGS settings //
 
     const [today, setToday] = useState<Date | undefined>(undefined);
@@ -39,16 +46,32 @@ function TimeTracker() {
     const [records, setRecords] = useState<Record[]>([]);
     const [refresh, setRefresh] = useState<boolean>(false);
 
+    // saved Elements //
+    const superContainer = useRef<HTMLInputElement>(null);
+
+
+    // Virtual scrolling 
+    const [visibleRecords, setVisibleRecords] = useState<Record[]>([]);
+    const visibleCount = 24; // amount of visible records in the realList container
+    const additionalCount = 4; // amount of additional loaded records for smooth loading
+
+    // const scrollRef = useRef(null);
+    const [scrollPos,setScrollPos] = useState<number>(0)
+    
+
+    // const scroll = superContainer.addEventListener('scroll', ...);
+    function scrollHandle() {
+        if (scrollRef){
+
+        }
+    }
+
+
     // Calculating delta of all the worked hours from the records
     const finalDelta = useMemo(() => {
         return records.reduce((acc, current) => acc + (current.out.getTime() - current.in.getTime()), 0);
     }, [records])
     // const [delta,setDelta] = useState<number>(0);
-
-    // saved Elements //
-
-    const superContainer = useRef<HTMLInputElement>(null);
-
 
 
 
@@ -67,19 +90,29 @@ function TimeTracker() {
             // the same as V
             const parsedRecords = parseRecords(savedRecords)
             setRecords(parsedRecords);
+            setVisibleRecords(parsedRecords.slice(0, visibleCount + additionalCount));
         }
+
+
     }, [refresh])
+
+
 
     // Height of superContainer depends on records.length
     useEffect(() => {
         if (superContainer.current) {
             superContainer.current.style.height = (records.length * 24) + "px";
         }
+
+        console.log(visibleRecords);
     }, [records])
 
 
 
     // MAIN PART //
+
+    // Button functionality
+    // Refresh button is written-in, just refreshes a refresh state to reload everything
 
     function checkInOut() {
         const newDate = new Date();
@@ -114,8 +147,10 @@ function TimeTracker() {
 
     }
 
+    /// TECH FUNCITONS ///
+
     // Parsing JSON records to the date format in and out
-    function parseRecords(records:string):Record[]{
+    function parseRecords(records: string): Record[] {
         const parsedRecordsRAW = JSON.parse(records);
         const parsedRecords = parsedRecordsRAW.map((el: any) => ({ "in": new Date(el.in), "out": new Date(el.out) }));
         return parsedRecords;
@@ -148,26 +183,34 @@ function TimeTracker() {
 
 
                         {/* ADDED RECORDS */}
-                        {records.map((record, index) => {
+                        {/* {records.map((record, index) => {
 
                             const rIn = record.in;
                             const rOut = record.out;
                             const delta: number = rOut.getTime() - rIn.getTime();
                             const timeDelta = millToTime(delta);
-                            // const hours = timeDelta.hours;
-                            // const minutes = timeDelta.minutes;
-
-
 
                             return <span key={index}><p>{rIn.getDate().toString().padStart(2, "0") + "." + (rIn.getMonth() + 1).toString().padStart(2, "0") + "." + (rIn.getFullYear() % 100).toString()}</p>
                                 <p>{rIn.getHours().toString().padStart(2, "0") + ":" + rIn.getMinutes().toString().padStart(2, "0")}</p>
                                 <p>{rOut.getHours().toString().padStart(2, "0") + ":" + rOut.getMinutes().toString().padStart(2, "0")}</p>
                                 <p>{timeDelta}</p></span>
                         })
+                        } */}
 
+                        {visibleRecords.map((record, index) => {
 
+                            const rIn = record.in;
+                            const rOut = record.out;
+                            const delta: number = rOut.getTime() - rIn.getTime();
+                            const timeDelta = millToTime(delta);
 
+                            return <span key={index}><p>{rIn.getDate().toString().padStart(2, "0") + "." + (rIn.getMonth() + 1).toString().padStart(2, "0") + "." + (rIn.getFullYear() % 100).toString()}</p>
+                                <p>{rIn.getHours().toString().padStart(2, "0") + ":" + rIn.getMinutes().toString().padStart(2, "0")}</p>
+                                <p>{rOut.getHours().toString().padStart(2, "0") + ":" + rOut.getMinutes().toString().padStart(2, "0")}</p>
+                                <p>{timeDelta}</p></span>
+                        })
                         }
+
                     </div>
                 </div>
 
